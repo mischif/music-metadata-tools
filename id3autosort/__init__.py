933ba3943e700c1bb3d907fd743565c63f57bd0a
Jeremy Brown <mischif@users.noreply.github.com>
2009-03-31 17:15:40 -0400

    Initial commit

---
+++ id3autosort/__init__.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+################################################################################
+#                             music-metadata-tools                             #
+#  A collection of tools for manipulating and interacting with music metadata  #
+#                               (C) 2009 Mischif                               #
+################################################################################
+
+__version__ = '1.0.0'
---
+++ setup.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+################################################################################
+#                             music-metadata-tools                             #
+#  A collection of tools for manipulating and interacting with music metadata  #
+#                               (C) 2009 Mischif                               #
+################################################################################
+
+from setuptools import setup
+
+setup(
+	name="music-metadata-tools",
+
+	version="0.0.1",
+
+	packages=["id3autosort"],
+
+	classifiers=[
+		"Development Status :: 4 - Beta",
+
+		"Operating System :: OS Independent",
+
+		"Programming Language :: Python :: 2",
+		"Programming Language :: Python :: 2.7",
+		],
+
+	keywords="ID3",
+	)
